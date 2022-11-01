91cc848a049a11b3979eccebb1f15d96af59be1b
Bernard Muli <bernardnthiwa@gmail.com>
2022-11-01 14:39:06 +0300

    Version-1

---
+++ index.js
@@ -0,0 +1,44 @@
+//function 
+const root = document.getElementById("root")
+const wrapper = document.createElement("div")
+//const box = `<div></div>` //cannot use append method on this HTML template
+wrapper.style.cssText = `display: grid; grid-template-columns: repeat(16, 1fr); width: 400px; height: 400px;`
+
+for(let i = 0; i < 256; i++){
+    const etch = document.createElement("div")
+    etch.style.cssText = `border: 1px black solid;`
+    etch.addEventListener("mouseover", (e) =>{
+    let r = getRandomNumber()
+    let b = getRandomNumber()
+    let g = getRandomNumber()
+    e.target.style.backgroundColor = `rgb(${r},${g},${b})`
+})
+wrapper.appendChild(etch)
+}
+root.appendChild(wrapper)
+
+const resetBtn = document.getElementById("reset-btn")
+resetBtn.addEventListener("click", () =>{
+    root.innerHTML = ""
+    const gridcols = Number.parseInt(prompt("Please enter a number between 1 and 100: "))
+    const gridSize = gridcols ** 2
+    const wrapperOne = document.createElement("div")
+    wrapperOne.style.cssText = `display: grid; grid-template-columns: repeat(${gridcols}, 1fr); width: 400px; height: 400px;`
+    for(let i = 0; i < gridSize; i++){
+        const etch = document.createElement("div")
+        etch.style.cssText = `border: 1px black solid;`
+        etch.addEventListener("mouseover", (e) =>{
+        let r = getRandomNumber()
+        let b = getRandomNumber()
+        let g = getRandomNumber()
+        e.target.style.backgroundColor = `rgb(${r},${g},${b})`
+    })
+    wrapperOne.appendChild(etch)
+    }
+    root.appendChild(wrapperOne)
+
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * 256)
+}
